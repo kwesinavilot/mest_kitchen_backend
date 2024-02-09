ba40dd21b0df96c4b5d3530eb56622e95e7fae9a
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-02-09 11:40:18 +0000

    Created first migration to prod db server

---
+++ prisma/migrations/20240209113716_init/migration.sql
@@ -0,0 +1,117 @@
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" SERIAL NOT NULL,
+    "orgID" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "altNumber" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Staff" (
+    "id" SERIAL NOT NULL,
+    "staffID" TEXT NOT NULL,
+    "orgID" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "password" TEXT NOT NULL DEFAULT 'password',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Staff_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Member" (
+    "id" SERIAL NOT NULL,
+    "memberID" TEXT NOT NULL,
+    "orgID" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "otherNames" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "nationality" TEXT NOT NULL,
+    "preferences" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PickupRequest" (
+    "id" SERIAL NOT NULL,
+    "requestID" TEXT NOT NULL,
+    "orgID" TEXT NOT NULL,
+    "assignerID" TEXT NOT NULL,
+    "collectorID" TEXT NOT NULL,
+    "mealType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PickupRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "adminID" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "password" TEXT NOT NULL DEFAULT 'password',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_orgID_key" ON "Organization"("orgID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_email_key" ON "Organization"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Staff_staffID_key" ON "Staff"("staffID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Staff_email_key" ON "Staff"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Member_memberID_key" ON "Member"("memberID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Member_email_key" ON "Member"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PickupRequest_requestID_key" ON "PickupRequest"("requestID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_adminID_key" ON "Admin"("adminID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
+
+-- AddForeignKey
+ALTER TABLE "Staff" ADD CONSTRAINT "Staff_orgID_fkey" FOREIGN KEY ("orgID") REFERENCES "Organization"("orgID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_orgID_fkey" FOREIGN KEY ("orgID") REFERENCES "Organization"("orgID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PickupRequest" ADD CONSTRAINT "PickupRequest_assignerID_fkey" FOREIGN KEY ("assignerID") REFERENCES "Member"("memberID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PickupRequest" ADD CONSTRAINT "PickupRequest_collectorID_fkey" FOREIGN KEY ("collectorID") REFERENCES "Member"("memberID") ON DELETE RESTRICT ON UPDATE CASCADE;
