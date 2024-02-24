edfc9454293f54cfdc56c60bb700d1e3ddccab41
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-02-24 02:36:23 +0000

    Straightened out the table for Members

--- app/(panel)/members/page.js
+++ app/(panel)/members/page.js
@@ -74,26 +74,51 @@ const eitDirectory = [
     email: 'suad.adbulraman@meltwater.com',
     level: 'regular',
   },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png',
+    name: 'Andrews Kwesi Ankomahene',
+    email: 'andrews.ankomahene@meltwater.com',
+    level: 'regular',
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
+    name: 'Obehi Omo-Ataga',
+    email: 'obehi.omoataga@meltwater.com',
+    level: 'strict',
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-7.png',
+    name: 'Savlin Wachire',
+    email: 'savlin.wachire@meltwater.com',
+    level: 'regular',
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-3.png',
+    name: 'Mohammadou Kaba',
+    email: 'mohammadou.kaba@meltwater.com',
+    level: 'serious',
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-6.png',
+    name: 'Suad Abdulraman',
+    email: 'suad.adbulraman@meltwater.com',
+    level: 'regular',
+  },
 ];
 
 export default function AllMembers() {
   return (
     <Container size="xl" bg="transparent">
-      <SimpleGrid
-        cols={{ base: 1, sm: 2 }}
-        spacing={{ base: 10, sm: 'xl' }}
-        verticalSpacing={{ base: 'md', sm: 'xl' }}
-        px="sm"
-        className={classes.simpleGrid}
-      >
-        <Paper shadow="xs" withBorder p="xl" radius="md">
-          <Title order={3} c="cyan.6">EIT Directory</Title>
+        <Title order={3}>EIT Directory</Title>
 
-          <Container mt="sm" fluid p={0}>
+        <Paper shadow="xs" withBorder mt="xl" radius="md" py="lg" px={0}>
             <EITListing data={eitDirectory} />
-          </Container>
         </Paper>
-      </SimpleGrid>
     </Container>
   );
 }
--- components/mass/EITListings.js
+++ components/mass/EITListings.js
@@ -26,8 +26,12 @@ export function EITListing({ data }) {
                     <Text fz="sm" fw={500}>
                         {item.name}
                     </Text>
+                </Link>
+            </Table.Td>
 
-                    <Text fz="xs" c="dimmed">{item.email}</Text>
+            <Table.Td>
+                <Link href={`/members`} className={classes.link}>
+                    <Text>{item.email}</Text>
                 </Link>
             </Table.Td>
 
@@ -42,24 +46,25 @@ export function EITListing({ data }) {
     ));
 
     return (
-        <ScrollArea h={630} offsetScrollbars>
+        <ScrollArea.Autosize mah={1100} offsetScrollbars>
             <Table
                 highlightOnHover
                 stickyHeader
                 withRowBorders={false}
-                horizontalSpacing="xs"
-                verticalSpacing="md"
+                horizontalSpacing="xl"
+                verticalSpacing="sm"
             >
                 <Table.Thead>
                     <Table.Tr>
                         <Table.Th className={classes.thead}></Table.Th>
                         <Table.Th className={classes.thead}>Full Name</Table.Th>
+                        <Table.Th className={classes.thead}>Email</Table.Th>
                         <Table.Th className={classes.thead}>Preference Level</Table.Th>
                     </Table.Tr>
                 </Table.Thead>
 
                 <Table.Tbody>{rows}</Table.Tbody>
             </Table>
-        </ScrollArea>
+        </ScrollArea.Autosize>
     );
 }
